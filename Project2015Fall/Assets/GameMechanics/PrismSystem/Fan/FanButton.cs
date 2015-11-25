4555a1330d4fa9c15cd2f65223cb73421da9e223
TakoJoyce <takojoyce@gmail.com>
2015-11-25 18:11:46 +0800

    First complete prototype commit

---
+++ Project2015Fall/Assets/GameMechanics/PrismSystem/Fan/FanButton.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class FanButton : MonoBehaviour
+{
+    public Sprite pressed, unpressed;
+    public GameObject fanLeavesObject;
+    public AreaEffector2D windZone;
+    public ParticleSystem windParticle;
+    public float windMagnitudeLow = 100,
+                    windMagnitudeMid = 200,
+                    windMagnitudeHigh = 300,
+                    windParticleLow = 3,
+                    windParticleMid = 8,
+                    windParticleHigh = 16,
+                    windSpeedLow = 5,
+                    windSpeedMid = 8,
+                    windSpeedHigh = 16;
+    public float allowedBias = 25;
+
+    SpriteRenderer  sRenderer;
+    PrismController prismController;
+
+    short pressWeight = 0;  //有多少player站在按鈕上？
+
+    [SerializeField]bool _isPressed = false;
+    public bool isPressed
+    {
+        get { return _isPressed; }
+        set { _isPressed = value; sRenderer.sprite = isPressed ? pressed : unpressed; }
+    }
+    
+    void Awake()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player" && collider.GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            pressWeight++;
+
+            if (!isPressed)
+            {
+                isPressed = true;
+                int numOfSpinningLeaves = 0;
+                foreach (FanLeaf f in fanLeavesObject.GetComponentsInChildren<FanLeaf>())
+                {
+                    f.OnButtonPressed();
+                    if (f.isSpinning)
+                        numOfSpinningLeaves++;
+                }
+                switch (numOfSpinningLeaves)
+                {
+                    case 0:
+                        windZone.forceMagnitude = 0;
+                        windParticle.emissionRate = 0;
+                        break;
+                    case 1:
+                        windZone.forceMagnitude = windMagnitudeLow;
+                        windParticle.emissionRate = windParticleLow;
+                        windParticle.startSpeed = windSpeedLow;
+                        break;
+                    case 2:
+                        windZone.forceMagnitude = windMagnitudeMid;
+                        windParticle.emissionRate = windParticleMid;
+                        windParticle.startSpeed = windSpeedMid;
+                        break;
+                    case 3:
+                        OnLeavesAllOpen();
+                        break;
+                    default:
+                        windZone.forceMagnitude = 0;
+                        windParticle.emissionRate = 0;
+                        break;
+                }
+            }
+        }
+    }
+
+    void OnLeavesAllOpen()
+    {
+        //計算三片扇葉的角度差，如果其中兩個的數字在120±bias的範圍內的話，就把風扇的強度調到high
+        FanLeaf[] fanLeaves = fanLeavesObject.GetComponentsInChildren<FanLeaf>();
+        float delta1 = Quaternion.Angle(fanLeaves[0].transform.rotation, fanLeaves[1].transform.rotation);
+        float delta2 = Quaternion.Angle(fanLeaves[1].transform.rotation, fanLeaves[2].transform.rotation);
+        float delta3 = 360f - delta1 - delta2;
+        short acceptableCount = 0;
+        if (Mathf.Abs(delta1 - 120f) < allowedBias) acceptableCount++;
+        if (Mathf.Abs(delta2 - 120f) < allowedBias) acceptableCount++;
+        if (Mathf.Abs(delta3 - 120f) < allowedBias) acceptableCount++;
+        if (acceptableCount >= 2)
+        {
+            windZone.forceMagnitude = windMagnitudeHigh;
+            windParticle.emissionRate = windParticleHigh;
+            windParticle.startSpeed = windSpeedHigh;
+        }
+        else
+        {
+            windZone.forceMagnitude = windMagnitudeMid;
+            windParticle.emissionRate = windParticleMid;
+            windParticle.startSpeed = windSpeedMid;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            pressWeight--;
+
+            if (pressWeight < 1)
+            {
+                isPressed = false;
+            }
+        }
+    }
+}
---
+++ Project2015Fall/Assets/GameMechanics/PrismSystem/MagicObject.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Flags]
+public enum MagicColor : byte {Red = 1, Green = 2, Blue = 4,
+                        Yellow = Red | Green, Cyan = Green | Blue, Magenta = Blue | Red,
+                        White = Red | Green | Blue, None = 0};
+
+public abstract class MagicObject : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+    protected static PrismController prismController;
+
+    protected bool interactable = true;
+    protected bool isVisible = false;
+
+    protected GameObject detachedObj;    //分離出來的物體的參考，以便讓derived class可以存取他
+
+    [SerializeField]
+    MagicColor _magicColor;
+    public MagicColor magicColor
+    {
+        get
+        {
+            return _magicColor;
+        }
+        set
+        {
+            _magicColor = value;
+            ChangeRendererColor(value);
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        ChangeRendererColor(_magicColor);
+    }
+
+    protected virtual void Start()
+    {
+        if (prismController == null)
+            prismController = GameObject.FindWithTag("StageManager").GetComponent<PrismController>();
+        prismController.RegisterMagicObject(this);
+
+    }
+
+    protected virtual void OnDestroy()
+    {
+        prismController.DeregisterMagicObject(this);
+    }
+
+    void ChangeRendererColor(MagicColor color)
+    {
+        Color rendererColor = spriteRenderer.color;
+        if ((magicColor & MagicColor.Red) != 0)
+            rendererColor.r = 1.0f;
+        else
+            rendererColor.r = 0.0f;
+        if ((magicColor & MagicColor.Green) != 0)
+            rendererColor.g = 1.0f;
+        else
+            rendererColor.g = 0.0f;
+        if ((magicColor & MagicColor.Blue) != 0)
+            rendererColor.b = 1.0f;
+        else
+            rendererColor.b = 0.0f;
+        spriteRenderer.color = rendererColor;
+    }
+
+    public virtual void OnPrismChange(MagicColor mode)
+    {
+        detachedObj = null; //先歸零
+
+        if (magicColor == mode)
+        {
+            BecomeInteractable();
+            return;
+        }
+
+        if ((magicColor & mode) != 0)
+        {
+            //減掉當前模式的顏色
+            //subtract color of mode
+            /*  true table:
+                old color: 0 1 0 1
+                subtract : 0 0 1 1
+                result   : 0 1 0 0  */
+
+            magicColor = magicColor & (MagicColor.White ^ mode) & magicColor;
+
+            detachedObj = (GameObject)Instantiate(gameObject, transform.position, transform.rotation);
+            detachedObj.transform.SetParent(transform.parent);
+            MagicObject mo = detachedObj.GetComponent<MagicObject>();
+            mo.magicColor = mode;
+            mo.BecomeInteractable();
+        }
+
+        BecomeNotInteractable();
+    }
+
+    public virtual void OnPrismClose()
+    {
+        if (!interactable)
+        {
+            BecomeInteractable();
+            //偵測重疊的物體
+            RaycastHit2D[] hits;
+            hits = Physics2D.LinecastAll(transform.position, transform.position);
+            foreach (RaycastHit2D hit in hits)
+            {
+                //排除偵測到自己的狀況
+                if (hit.transform == transform)
+                    continue;
+                //如果偵測到的物體是可以互動的MagicObject，把它的顏色加到自己身上，然後摧毀它。
+                MagicObject mo;
+                mo = hit.transform.GetComponent<MagicObject>();
+                if ((mo != null) && mo.interactable)
+                {
+                    magicColor = magicColor | mo.magicColor;
+                    Destroy(mo.gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+    public virtual void BecomeInteractable()
+    {
+        interactable = true;
+    }
+
+    public virtual void BecomeNotInteractable()
+    {
+        interactable = false;
+    }
+
+    void OnBecameInvisible() { isVisible = true; }
+
+    void OnBecameVisible() { isVisible = false; }
+}
